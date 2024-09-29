b6f761e2d39241da2277dfdcd0d875ca58a16efa
Eleazar Orono <eorono@gmail.com>
2024-09-29 12:01:30 -0400

    Rules and validation failed message declare and created

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
 
 class UpdateProductRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,18 @@ class UpdateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'details' => 'required'
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new \HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation Errors',
+            'data' => $validator->errors()
+
+        ]));
     }
 }
